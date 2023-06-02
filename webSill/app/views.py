14f7cf5af7853125bad4df8bf5e4c532630c43bc
nguyennhuhha <nhuha3110@gmail.com>
2023-06-02 22:32:03 +0700

    update 5th

--- webSill/app/views.py
+++ webSill/app/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def base(request):
+    '''
+    Trả về base (header & footer)
+    '''
+    context={}
+    return render(request, 'app/base.html',context) 
 def home(request):
-    return render(request,'app/base.html')
+    """
+        Trả về trang chủ home
+    """
+    context ={}
+    return render(request,'app/home.html',context)
